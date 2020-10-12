d2f9cc8248b8b3b95ba89bf76cc7a4a181612249
Obinna Okafor <okaforobinna7@gmail.com>
2020-10-12 18:52:36 +0100

    Updates

---
+++ precache-manifest.d2b59a754779f7d742936332a9a0c2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea15c77a10ccb02c06699c5d99dc7cc",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "594a26557e24f776e042",
+    "url": "/drum-machine/static/css/main.64791871.chunk.css"
+  },
+  {
+    "revision": "1e0ae55e4f683b15c137",
+    "url": "/drum-machine/static/js/2.3739f3cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.3739f3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594a26557e24f776e042",
+    "url": "/drum-machine/static/js/main.722a78c6.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
